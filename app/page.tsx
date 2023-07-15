b1b6e2b66a7b0aead3132f009c497da29f9ec5ca
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2023-07-15 15:39:24 -0300

    Next middleware (#4)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
 import Link from 'next/link'
-import { redirect } from 'next/navigation'
-import { getUserSession } from '~/lib/session'
 
 export default async function Home() {
-  const user = await getUserSession()
-
-  if (user) return redirect('/me')
-
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-2xl font-bold">Calendext</h1>
